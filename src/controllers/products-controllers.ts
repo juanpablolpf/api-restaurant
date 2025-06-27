fcf3a2446236cf07a55d7881f5463233f2d29b82
Juan Pablo Filho <juan.pablo290102@gmail.com>
2025-06-27 10:41:58 -0300

    creating the exception handling middleware

--- src/controllers/products-controllers.ts
+++ src/controllers/products-controllers.ts
@@ -1,9 +1,9 @@
 import {NextFunction, Request, Response} from "express"
+import { AppError } from "@/utils/AppError"
 
 class ProductCrontroller {
     async index(request: Request, response: Response, next: NextFunction){
         try {
-           
             return response.json({message: "ok"})
         } catch (error) {
             next(error)
