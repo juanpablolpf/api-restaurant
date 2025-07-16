50c5b511105647c893233234edab8f974390a444
Juan Pablo Filho <juan.pablo290102@gmail.com>
2025-07-16 11:56:08 -0300

    saving order

--- src/controllers/orders-controller.ts
+++ src/controllers/orders-controller.ts
@@ -34,7 +34,14 @@ class OrdersController {
                 throw new AppError("product not found")
             }
 
-            return response.status(201).json(product)
+            await knex<OrderRepository>("orders").insert({
+                table_session_id,
+                product_id,
+                quantity,
+                price: product.price 
+            })
+
+            return response.status(201).json()
         } catch (error) {
             next(error)
         }
